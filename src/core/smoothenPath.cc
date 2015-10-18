81a2b7caed1e92fd2294946bd6e77ec952ba1bd3
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-10-18 16:40:32 +0200

    Fix smoothen path

--- src/core/smoothenPath.cc
+++ src/core/smoothenPath.cc
@@ -1,34 +1,42 @@
 #include "smoothenPath.hpp"
 
 vector<vector<int>> smoothenPath(vector<vector<int>>* path) {
-    vector<vector<int>> newPath(path->size(), vector<int>(2));
+    vector<vector<int>> newPath(path->size(), vector<int>(2, 0));
 
     newPath[0] = path->at(0);
-    int i = 1;
+    int l = 1;
 
     change change = CHANGE_UNKNOWN;
 
     for (unsigned int i = 1; i < path->size(); ++i) {
+        int cmpX = newPath[l - 1][0];
+        int cmpY = newPath[l - 1][1];
+        int pX   = (*path)[i][0];
+        int pY   = (*path)[i][1];
+
+
         if (change == CHANGE_UNKNOWN) {
-            change = (newPath[newPath.size() - 1][0] == (*path)[i][0]) ? CHANGE_Y : CHANGE_X;
+            change = (cmpX == pX) ? CHANGE_Y : CHANGE_X;
         }
 
         if (change == CHANGE_Y) {
-            if (newPath[newPath.size() - 1][0] != (*path)[i][0]) {
+            if (cmpX != pX) {
                 change = CHANGE_UNKNOWN;
-                newPath[i] = path->at(i - 1);
-                ++i;
+                newPath[l] = (*path)[i - 1];
+                ++l;
             }
         } else {
-            if (newPath[newPath.size() - 1][1] != (*path)[i][1]) {
+            if (cmpY != pY) {
                 change = CHANGE_UNKNOWN;
-                newPath[i] = path->at(i - 1);
-                ++i;
+                newPath[l] = (*path)[i - 1];
+                ++l;
             }
         }
     }
 
-    newPath[i] = path->at(path->size() - 1);
+    newPath[l] = path->at(path->size() - 1);
+
+    newPath.resize(l);
 
     return newPath;
 }
